f5a3cb4b5be9d14f3a7eec2279e78cf506329c9d
dorianinc <dorianrdba@gmail.com>
2023-03-29 15:42:23 -0700

    completed long practice

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,37 +1,47 @@
-import React from 'react';
+import React from "react";
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
-import './ProductView.css';
-import { useState } from 'react';
+import "./ProductView.css";
+import { useState, useEffect } from "react";
 
 function ProductView({ products }) {
-
   const [sideOpen, setSideOpen] = useState(true);
-  const [product, setProduct] = useState({});
+  const [selectedProduct, setSelectedProduct] = useState(0);
+
+  useEffect(() => {
+    console.log(`selectedProduct CHANGED TO`, selectedProduct);
+    if (selectedProduct)
+      setSideOpen(true);
+  }, [selectedProduct]);
+
+  useEffect(() => {
+    console.log(`sideOpen CHANGED TO`, sideOpen);
+    if (!sideOpen)
+      setSelectedProduct();
+  }, [sideOpen]);
 
   return (
     <div className="product-view">
       <div className="product-main-area">
         <h1>Products</h1>
         <div className="product-list">
-          {products.map(item =>
+          {products.map((item) => (
             <ProductListItem
               key={item.id}
               product={item}
-              isSelected={item.id === product.id}
-              onClick={() => setProduct({...item})}
+              isSelected={selectedProduct ? item.id === selectedProduct.id : false}
+              onClick={() => setSelectedProduct({ ...item })}
             />
-          )}
+          ))}
         </div>
       </div>
       <div className="product-side-panel">
         <div className="product-side-panel-toggle-wrapper">
-          <div className="product-side-panel-toggle"
-               onClick={() => setSideOpen((prev)=>!prev)}>
-            {sideOpen ? '>' : '<'}
+          <div className="product-side-panel-toggle" onClick={() => setSideOpen((prev) => !prev)}>
+            {sideOpen ? ">" : "<"}
           </div>
         </div>
-        <ProductDetails visible={sideOpen} product={product}/>
+        <ProductDetails visible={sideOpen} product={selectedProduct} />
       </div>
     </div>
   );
